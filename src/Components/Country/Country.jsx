dbce01e8b39c06541bce3714cbf37cd5f3384c8a
rifat3790 <mdrifayethossen@gmail.com>
2023-09-10 14:51:52 -0700

    visited country added

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,5 +1,5 @@
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
     // console.log(country);
     const {name,flags} = country;
     // console.log(name);
@@ -8,18 +8,18 @@ const Country = ({country}) => {
         <div>
             <div className="card bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-            <img src={flags.png} alt="Shoes" className="rounded-xl h-[200px]" />
+            <img src={flags.png} alt="Shoes" className="rounded-xl h-[200px] w-full" />
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">{name.common}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            
             <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+            <button onClick={()=> handleVisitedCountries(country)} className="btn btn-primary">Mark Visited</button>
             </div>
         </div>
         </div>
         </div>
-        
+        // clicked add
     );
 };
 
--- src/Components/Cuntries/Countries.jsx
+++ src/Components/Cuntries/Countries.jsx
@@ -6,6 +6,13 @@ const Countries = () => {
 
     const [countries, setCountries] = useState([]);
 
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) =>{
+        const newVisitedCountries =[...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
     useEffect( ()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
@@ -15,11 +22,17 @@ const Countries = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-10">Country: {countries.length}</h2>
-            <div className="container max-w-screen-xl mx-auto flex flex-col lg:flex-row">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="container mx-auto flex flex-col lg:flex-row">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 flex-1">
             {
-                countries.map(country=> <Country country={country} key={country.cca3} ></Country>)
+                countries.map(country=> <Country country={country} handleVisitedCountries={handleVisitedCountries} key={country.cca3} ></Country>)
             }
+            </div>
+            <div className="ml-5">
+                <h3 className="font-medium text-center flex-1">Visited Countries: {visitedCountries.length}</h3>
+                {
+                    visitedCountries.map(country =><li key={country.cca3}>{country.name.common}</li>)
+                }
             </div>
             </div>
         </div>
