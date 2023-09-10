ab2a0390b0acd2c2052b747d8bbb73574e4d4552
rifat3790 <mdrifayethossen@gmail.com>
2023-09-10 15:37:58 -0700

    some modify

--- src/Components/Cuntries/Countries.jsx
+++ src/Components/Cuntries/Countries.jsx
@@ -22,7 +22,7 @@ const Countries = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-10">Country: {countries.length}</h2>
-            <div className="container mx-auto flex flex-col lg:flex-row">
+            <div className="container max-w-screen-xl mx-auto flex flex-col lg:flex-row">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 flex-1">
             {
                 countries.map(country=> <Country country={country} handleVisitedCountries={handleVisitedCountries} key={country.cca3} ></Country>)
